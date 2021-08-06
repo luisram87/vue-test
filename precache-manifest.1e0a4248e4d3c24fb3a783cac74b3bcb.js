3e3dd260a9ec22bb9acd2584703e646e3dd23442
Luis Cruz <luisram06@gmail.com>
2021-08-06 09:28:42 -0500

    first commit

---
+++ precache-manifest.1e0a4248e4d3c24fb3a783cac74b3bcb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ea5a1edbeed20252e4",
+    "url": "/vue-test/css/app.4d08c71d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-test/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2bb435e0bc893ab1b4fc2fbac3ea37b7",
+    "url": "/vue-test/index.html"
+  },
+  {
+    "revision": "3b2b5a41d346ce6f96d0",
+    "url": "/vue-test/js/about.2b8983e6.js"
+  },
+  {
+    "revision": "c5ea5a1edbeed20252e4",
+    "url": "/vue-test/js/app.1a1fbe54.js"
+  },
+  {
+    "revision": "375511d08c9cd53db48c",
+    "url": "/vue-test/js/chunk-vendors.3cbed209.js"
+  },
+  {
+    "revision": "071b9b4216b78999b36b6808ec28c6f0",
+    "url": "/vue-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-test/robots.txt"
+  }
+]);
